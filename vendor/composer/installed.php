17403ddaf28214b4a09057990f8b8b992fc55bf5
Gabriel Martins <gabrielm@castrolanda.coop.br>
2022-03-30 10:55:20 -0300

    Primeiro commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'coffeecode/cropper' => array(
+            'pretty_version' => '1.3.4',
+            'version' => '1.3.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../coffeecode/cropper',
+            'aliases' => array(),
+            'reference' => '4c98fa8601ad1dcb3d94a605e7bfa36b7ac5b87e',
+            'dev_requirement' => false,
+        ),
+        'rosell-dk/webp-convert' => array(
+            'pretty_version' => '2.5.0',
+            'version' => '2.5.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../rosell-dk/webp-convert',
+            'aliases' => array(),
+            'reference' => 'b349da7b6e04a4c269bbbbf7c279e0e433a71fd0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
